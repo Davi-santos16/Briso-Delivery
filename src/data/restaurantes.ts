ce376d63e2e34263bf48db3eba40045b5e9c1519
davi <davicastro213@gmail.com>
2025-06-11 23:14:30 -0300

    feat:atualizaçao

--- src/data/restaurantes.ts
+++ src/data/restaurantes.ts
@@ -1,43 +1,206 @@
 export const restaurantes = [
   {
     id: "1",
-    nome: "Pizzaria do davi",
-    categoria: "Pizza",
-    foto: "/img/restaurantes/c0f31c78-832e-4713-ab55-68b1ec1f9294.png",
+    nome: "Britto's Pizzaria",
+    categoria: "Pizzaria",
+    foto: "/img/restaurantes/britos/Britos.png",
     tempoEntrega: "30-40 min",
     avaliacao: 4.8,
-    telefone: "5588981724489",
+    telefone: "558881116293",
     menu: [
       {
         id: "101",
-        nome: "Pizza doce que nao e doce",
-        descricao: "Molho de tomate, mussarela fresca e manjericão",
-        foto: "/imagens/margherita.jpg",
+        nome: "MUSSARELA",
+        descricao: "MUSSARELA",
+        foto: "/img/restaurantes/britos/cardapio/pizzas/Mozzarela.webp",
         categoria: "Pizzas",
         tamanhos: [
-          
-          { id: "101-G", tamanho: "Grande", preco: 49.90 }
+          { id: "101-M", tamanho: "Média", preco: 35.00 },
+          { id: "101-G", tamanho: "Grande", preco: 40.00 }
         ]
       },
       {
         id: "102",
         nome: "Pizza Calabresa",
-        descricao: "Molho de tomate, mussarela, calabresa e cebola",
-        foto: "/imagens/calabresa.jpg",
+        descricao: "MUSSARELA, CALABRESA E CEBOLA",
+        foto: "/img/restaurantes/britos/cardapio/pizzas/Calabresa.avif",
         categoria: "Pizzas",
         tamanhos: [
-          { id: "102-P", tamanho: "Pequena", preco: 29.90 },
-          { id: "102-G", tamanho: "Grande", preco: 49.90 }
+          { id: "102-M", tamanho: "Média", preco: 37.00  },
+          { id: "102-G", tamanho: "Grande", preco: 42.00 }
+        ]
+      },
+      
+      {
+        id: "103",
+        nome: "Mista",
+        descricao: "MUSSARELA E PRESUNTO",
+        foto: "/img/restaurantes/britos/cardapio/pizzas/Mista.jpg",
+        categoria: "Pizzas",
+        tamanhos: [
+          { id: "103-P", tamanho: "Média", preco: 37.00  },
+          { id: "103-G", tamanho: "Grande", preco: 42.00 }
+        ]
+      },
+
+      {
+        id: "104",
+        nome: "Pizza Marguerita",
+        descricao: "MUSSARELA, TOMATE, PARMESÃO E MANJERICÃO",
+        foto: "/img/restaurantes/britos/cardapio/pizzas/Calabresa.avif",
+        categoria: "Pizzas",
+        tamanhos: [
+          { id: "104-M", tamanho: "Média", preco: 37.00  },
+          { id: "104-G", tamanho: "Grande", preco: 42.00 }
         ]
       },
       {
-        id: "201",
-        nome: "Filé com Fritas",
-        descricao: "Filé bovino com batata frita crocante",
-        foto: "/imagens/file-fritas.jpg",
-        categoria: "Prato Principal",
-        preco: 39.90
+        id: "105",
+        nome: "Pizza Portuguesa",
+        descricao: "MUSSARELA, PRESUNTO, OVO, PIMENTÃO E CEBOLA",
+        foto: "/img/restaurantes/britos/cardapio/pizzas/Calabresa.avif",
+        categoria: "Pizzas",
+        tamanhos: [
+          { id: "105-M", tamanho: "Média", preco: 40.00  },
+          { id: "105-G", tamanho: "Grande", preco: 45.00 }
+        ]
       },
+      {
+        id: "106",
+        nome: "Pizza C/ Cheddar",
+        descricao: "MUSSARELA, CALABRESA, OVO E CHEDDAR",
+        foto: "/img/restaurantes/britos/cardapio/pizzas/Calabresa.avif",
+        categoria: "Pizzas",
+        tamanhos: [
+          { id: "106-M", tamanho: "Média", preco: 43.00  },
+          { id: "106-G", tamanho: "Grande", preco: 48.00 }
+        ]
+      },
+      {
+        id: "107",
+        nome: "Pizza Frango",
+        descricao: "MUSSARELA, FRANGO E REQUEIJÃO CREMOSO",
+        foto: "/img/restaurantes/britos/cardapio/pizzas/Calabresa.avif",
+        categoria: "Pizzas",
+        tamanhos: [
+          { id: "107-M", tamanho: "Média", preco: 40.00  },
+          { id: "107-G", tamanho: "Grande", preco: 45.00 }
+        ]
+      },
+
+
+       {
+        id: "109",
+        nome: "D`Aquele Jeito",
+        descricao: "MUSSARELA, PEPPERONI E TOMATE",
+        foto: "/img/restaurantes/britos/cardapio/pizzas/Calabresa.avif",
+        categoria: "Pizzas",
+        tamanhos: [
+          { id: "109-M", tamanho: "Média", preco: 50.00  },
+          { id: "109-G", tamanho: "Grande", preco: 55.00 }
+        ]
+      },
+      {
+        id: "110",
+        nome: "DO CHEFE",
+        descricao: "MUSSARELA, CALABRESA, CEBOLA, CLARA DE OVO, GORGONZOLA E PIMENTA CALABRESA",
+        foto: "/img/restaurantes/britos/cardapio/pizzas/Calabresa.avif",
+        categoria: "Pizzas",
+        tamanhos: [
+          { id: "110-M", tamanho: "Média", preco: 50.00  },
+          { id: "110-G", tamanho: "Grande", preco: 55.00 }
+        ]
+      },
+       {
+        id: "111",
+        nome: "TOSCANA",
+        descricao: "MUSSARELA, CALABRESA, CEBOLA, OVO,",
+        foto: "/img/restaurantes/britos/cardapio/pizzas/Calabresa.avif",
+        categoria: "Pizzas",
+        tamanhos: [
+          { id: "111-M", tamanho: "Média", preco: 40.00  },
+          { id: "111-G", tamanho: "Grande", preco: 45.00 }
+        ]
+      },
+      {
+        id: "112",
+        nome: "4 QUEIJOS",
+        descricao: "MUSSARELA, REQUEIJÃO CREMOSO PARMESÃO E GOROGONZOLA",
+        foto: "/img/restaurantes/britos/cardapio/pizzas/Calabresa.avif",
+        categoria: "Pizzas",
+        tamanhos: [
+          { id: "112-M", tamanho: "Média", preco: 47.00  },
+          { id: "112-G", tamanho: "Grande", preco: 52.00 }
+        ]
+      },
+        {
+        id: "113",
+        nome: "DA`CASA",
+        descricao: "MUSSARELA, BACON, ALHO E ALECRIM",
+        foto: "/img/restaurantes/britos/cardapio/pizzas/Calabresa.avif",
+        categoria: "Pizzas",
+        tamanhos: [
+          { id: "113-M", tamanho: "Média", preco: 40.00  },
+          { id: "113-G", tamanho: "Grande", preco: 45.00 }
+        ]
+      },
+      {
+        id: "114",
+        nome: "ICARAIZINHO",
+        descricao: "MUSSARELA, FRANGO, OVO E REQUEIJÃO CREMOSO",
+        foto: "/img/restaurantes/britos/cardapio/pizzas/Calabresa.avif",
+        categoria: "Pizzas",
+        tamanhos: [
+          { id: "114-M", tamanho: "Média", preco: 40.00  },
+          { id: "114-G", tamanho: "Grande", preco: 45.00 }
+        ]
+      },
+      {
+        id: "115",
+        nome: "ICARAIZINHO",
+        descricao: "MUSSARELA, FRANGO, OVO E REQUEIJÃO CREMOSO",
+        foto: "/img/restaurantes/britos/cardapio/pizzas/Calabresa.avif",
+        categoria: "Pizzas",
+        tamanhos: [
+          { id: "115-M", tamanho: "Média", preco: 40.00  },
+          { id: "115-G", tamanho: "Grande", preco: 45.00 }
+        ]
+      },
+      {
+        id: "116",
+        nome: "BRITTO`S",
+        descricao: "MUSSARELA, PRESUNTO, CALABRASA E BACON",
+        foto: "/img/restaurantes/britos/cardapio/pizzas/Calabresa.avif",
+        categoria: "Pizzas",
+        tamanhos: [
+          { id: "116-M", tamanho: "Média", preco: 42.00  },
+          { id: "116-G", tamanho: "Grande", preco: 47.00 }
+        ]
+      },
+       {
+        id: "117",
+        nome: "CARNE DO SOL",
+        descricao: "MUSSARELA, CARNE DE SOL, PIMENTÃO CEBOLA E REQUEIJÃO CREMOSO",
+        foto: "/img/restaurantes/britos/cardapio/pizzas/Calabresa.avif",
+        categoria: "Pizzas",
+        tamanhos: [
+          { id: "117-M", tamanho: "Média", preco: 52.00  },
+          { id: "117-G", tamanho: "Grande", preco: 57.00 }
+        ]
+      },
+       {
+        id: "117",
+        nome: "FRANGO CHEESE",
+        descricao: "MUSSARELA, FRANGO, BACON, MILHO E CREAM CHESSE",
+        foto: "/img/restaurantes/britos/cardapio/pizzas/Calabresa.avif",
+        categoria: "Pizzas",
+        tamanhos: [
+          { id: "117-M", tamanho: "Média", preco: 55.00  },
+          { id: "117-G", tamanho: "Grande", preco: 60.00 }
+        ]
+      },
+
+
       {
         id: "301",
         nome: "Coca-Cola 2L",
@@ -45,15 +208,8 @@ export const restaurantes = [
         foto: "/imagens/coca-cola.jpg",
         categoria: "Bebidas",
         preco: 9.90
-      },
-      {
-        id: "301",
-        nome: "Coca-Cola 2L",
-        descricao: "Refrigerante Coca-Cola 2 litros",
-        foto: "/imagens/coca-cola.jpg",
-        categoria: "Prato Principal",
-        preco: 9.90
       }
+     
     ]
   },
  
